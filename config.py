80e12cecedd721bbb40f5ce0aa867e4a547afa6b
Favour Iruoghene Agbaike <agbaikefavour@gmail.com>
2026-07-25 14:48:59 +0200

    Add config file

---
+++ config.py
@@ -0,0 +1,9 @@
+# Configuration file for CodeTrack
+import os
+AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY", "")
+DEBUG_MODE = False
+def get_config():
+    return {
+        "aws_access_key": AWS_ACCESS_KEY,
+        "debug": DEBUG_MODE
+    }
